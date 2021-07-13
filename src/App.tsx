41aa6248daa3fd02cc866f93ebb931e1fc0a3723
ya-galyuk <ya.galyuk@gmail.com>
2021-07-13 17:20:08 +0300

    Setup client
    - create simple form for sign in/up
    - add mobx store
    - some logic for token
    - setup cors on the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useEffect, useState} from 'react';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {observer} from "mobx-react-lite";
+import UserService from "./services/UserService";
+import {IUser} from "./models/IUser";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {store} = useContext(Context)
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            store.checkAuth()
+        }
+    }, [])
+
+    const getUsers = async () => {
+        try {
+            const response = await UserService.fetchUsers()
+            setUsers(response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    if (store.isLoading) {
+        return (<>Loading...</>)
+    }
+
+    if (!store.isAuth) {
+        return (<LoginForm/>)
+    }
+    return (
+        <div>
+            <h1>{
+                store.isAuth
+                    ? `User ${store.user.email} authorized`
+                    : 'You must log in'
+            }</h1>
+            <h2>{
+                store.user.isActivated
+                    ? `User ${store.user.email} is activated`
+                    : 'Email NOT activated'
+            }</h2>
+            <button onClick={() => store.logout()}>logout</button>
+
+            <div>
+                <button onClick={getUsers}>Get all users</button>
+            </div>
+            {users.map(user =>
+                <div key={user.email}>{user.email}</div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default observer(App);
